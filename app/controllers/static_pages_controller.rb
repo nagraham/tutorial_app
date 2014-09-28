e099cbe65973c8680d13d48c7371f3714849bcd1
Alexander Graham <nagraham00@hotmail.com>
2014-09-27 17:37:19 -0700

    Added the StaticPages controller and vies for home, help

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+
+  # Hitting /static_pages/home calls home;
+  # and because it inherits from ApplicationController,
+  # which calls this method and then renders the view,
+  # an empty controller method means only the view is displayed.
+  def home
+  end
+
+  def help
+  end
+end
