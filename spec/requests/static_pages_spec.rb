7f41726fee2159ddcc8ed14438f2c665a903a065
Alexander Graham <nagraham00@hotmail.com>
2014-09-27 18:49:15 -0700

    Added dynamic webpage titles to static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,12 +10,19 @@ describe 'Static pages' do
   describe 'Home page' do
 
     # RSPEC: it <your test comment> do
-    it 'should have the content \'Sample App\'' do
+    it 'should have the content \'Tutorial App\'' do
       # CAPYBARA is visiting the homepage
       visit '/static_pages/home'
       # CAPYBARA gives us the 'page' variable
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Tutorial App')
     end
+
+    it 'should have the right title' do
+      visit '/static_pages/home'
+      # don't need full title; you could also use any sub-string
+      expect(page).to have_title('Ruby on Rails Tutorial App | Home')
+    end
+
   end
 
   describe 'Help page' do
@@ -23,12 +30,22 @@ describe 'Static pages' do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it 'should have the right title' do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial App | Help')
+    end
   end
 
   describe 'About page' do
-    it 'should have the content \'About us\'' do
+    it 'should have the content \'About Us\'' do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial App | About Us')
     end
   end
 end
